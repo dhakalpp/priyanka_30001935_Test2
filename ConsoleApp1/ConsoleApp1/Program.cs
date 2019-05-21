04b21f13f131f3bac7f2a0a23936c0a6391c2d89
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-05-22 11:00:23 +1200

    test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Priyanka Dhakal*/
+/*Student Id = 30001935*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class salary
+
+    {
+        private Double Annual;
+        private Double weekly;
+
+        public double Annual;
+        get {return Annual;}
+        set {annual = value;}
+     }
+       {
+        public double weekly;
+        get {return weekly;}
+        set {weekly = value;}
+
+        public salary = {double_Annual, double_weekly}
+      } 
+
+    { 
+    Annual = _annual;
+    weekly = _weekly;
+    }
+
+    public string Income()
+    {
+    return "/your yearly income is $" + annual + "/your weekly salary is "$ + weekly
+    }
+       class wage
+       {
+    private double hourly;
+    private double weekly;
+
+    public Double Annual salary
+       }
+        private static object salary;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Payroll application");
+            double annualsalary = 80000;
+            double weeklysalary = 33.72;
+            salary p1 = new salary(annual weekly);
+            Console.WriteLine("your salary is :(p1. Annual per year./your weekly income is:$(p1.weekly) per week"
+
+            public double Annual { get; set; }
+
+           get {return annual;}
+            set {annual = value;}
+
+         public string  wages 
+        }
+    }
+}
